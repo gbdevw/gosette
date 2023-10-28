cbdc5c52ce2ca53f29017a88577872d5ac0629b2
gbdevw <guillaume.braaibant@lake42.eu>
2023-10-28 20:50:51 +0200

    Finalize testing

--- httptestserver_test.go
+++ httptestserver_test.go
@@ -1,32 +1,16 @@
-// # Description
-//
-// The package provides a HTTP server implementation which can be used to mock HTTP responses and
-// spy on incoming requests and outgoing responses. The primary goal of this package is to provide
-// an easy way to perform local end-to-end tests of HTTP clients against a local HTTP server.
-//
-// # Features
-//   - Built on top of http and httptest packages.
-//   - Easily add predefined HTTP responses.
-//   - Responses are served in a FIFO fashion until there is only one left: If only one response is
-//     available, it is served indefinitly. The server returns an empty 404 response when no
-//     predefined responses are available.
-//   - The server records HTTP requests, body and HTTP response in a FIFO fashion. These records can
-//     be extracted from the test server to spy on exchanged requests and responses.
-//   - In case the server encounter an error while processing the request or serving the predefined
-//     response, the server will reply with a 500 response with a text body that is the string
-//     representation of the error. The server will also add a record to its queue. The added record
-//     will have its ServerError set with an error which wraps the error that has occured.
-//   - Helper functions are available to clear responses and records.
-//   - Pluggable httptest.Server. The server handler will be overriden by the framework. The
-//     underlying httptest.Server is accessible so more experienced users can build more complex
-//     test cases (like shutting down client connections, testing with TLS, ...).
 package gosette
 
 import (
+	"bytes"
+	"fmt"
 	"io"
 	"net/http"
+	"net/http/httptest"
+	"net/url"
+	"strings"
 	"testing"
 
+	"github.com/stretchr/testify/mock"
 	"github.com/stretchr/testify/require"
 	"github.com/stretchr/testify/suite"
 )
@@ -83,7 +67,12 @@ func (suite *HTTPTestServerUnitTestSuite) TestWithSingleJsonResponse() {
 	}`
 	expectedStatusCode := http.StatusOK
 
-	// Push predefined response to server
+	// Content of request
+	payload := `{
+		"hello": "world"
+	}`
+
+	// Push a predefined response to test server
 	suite.hts.PushPredefinedServerResponse(&PredefinedServerResponse{
 		Status: expectedStatusCode,
 		Headers: map[string][]string{
@@ -98,7 +87,7 @@ func (suite *HTTPTestServerUnitTestSuite) TestWithSingleJsonResponse() {
 
 	// Send a HTTP request to server
 	// Server is expected to reply with the predefined response
-	resp, err := client.Get(suite.hts.server.URL)
+	resp, err := client.Post(suite.hts.server.URL, "application/json", strings.NewReader(payload))
 	require.NoError(suite.T(), err)
 	require.NotNil(suite.T(), resp)
 	// Check first response status code
@@ -114,22 +103,332 @@ func (suite *HTTPTestServerUnitTestSuite) TestWithSingleJsonResponse() {
 	record := suite.hts.PopServerRecord()
 	require.NoError(suite.T(), record.ServerError)
 	require.NotNil(suite.T(), record.Request)
-	require.Empty(suite.T(), record.RequestBody) // Request body is empty aas request is a GET
 	require.NotNil(suite.T(), record.Response)
+	// Check recorded request body against original payload
+	recReqBody, err := io.ReadAll(record.RequestBody)
+	require.NoError(suite.T(), err)
+	require.Equal(suite.T(), payload, string(recReqBody))
 	// Extract recorded response body and compare
 	recordedRespBody, err := io.ReadAll(record.Response.Result().Body)
 	require.NoError(suite.T(), err)
 	require.Equal(suite.T(), respBody, recordedRespBody)
 	// Compare request method & URL
 	require.Contains(suite.T(), suite.hts.server.URL, record.Request.Host)
-	require.Equal(suite.T(), http.MethodGet, record.Request.Method)
+	require.Equal(suite.T(), http.MethodPost, record.Request.Method)
 }
 
 // Test HTTPTestServer when form encoded data are provided in the incoming request. Test will
 // ensure test server handles and record well the request form data.
-func (suite *HTTPTestServerUnitTestSuite) TestWithFormEncodedData() {}
+func (suite *HTTPTestServerUnitTestSuite) TestWithFormEncodedData() {
 
-// Test HTTPTestServer when multiple predefined responses are defined. Test will ensure predefined
-// responses and records are served in a FIFO fashion. Test will also ensure last predefined
-// response is served indefinitly.
-func (suite *HTTPTestServerUnitTestSuite) TestWithMultipleResponses() {}
+	// Predefined form data & expectations
+	expectedStatusCode := http.StatusCreated
+	expectedUrlPath := "/form"
+	predefinedFormData := url.Values{
+		"id":       []string{"1"},
+		"messages": []string{"hello", "world"},
+	}
+
+	// Push a predefined response to test server
+	suite.hts.PushPredefinedServerResponse(&PredefinedServerResponse{
+		Status:  expectedStatusCode,
+		Headers: map[string][]string{},
+		Body:    nil,
+	})
+
+	// Get a http.Client which trusts the server certificate if TLS is enabled
+	client := suite.hts.Client()
+	require.NotNil(suite.T(), client)
+
+	// Send a POST request wiith form data
+	resp, err := client.PostForm(suite.hts.GetBaseURL()+expectedUrlPath, predefinedFormData)
+	require.NoError(suite.T(), err)
+	require.NotNil(suite.T(), resp)
+
+	// Check response
+	require.Equal(suite.T(), expectedStatusCode, resp.StatusCode)
+	respBody, err := io.ReadAll(resp.Body)
+	require.NoError(suite.T(), err)
+	require.Empty(suite.T(), respBody)
+
+	// Extract recorded request and response
+	srvrec := suite.hts.PopServerRecord()
+	require.NotNil(suite.T(), srvrec)
+
+	// Check request
+	require.Equal(suite.T(), http.MethodPost, srvrec.Request.Method)
+	require.Equal(suite.T(), expectedUrlPath, srvrec.Request.URL.String())
+	require.Equal(suite.T(), predefinedFormData, srvrec.Request.Form)
+
+	// Check recorded request body
+	recReqBody, err := io.ReadAll(srvrec.RequestBody)
+	require.NoError(suite.T(), err)
+	require.Equal(suite.T(), []byte(predefinedFormData.Encode()), recReqBody)
+
+	// Check recorded response
+	require.Equal(suite.T(), resp.StatusCode, srvrec.Response.Result().StatusCode)
+	recRespBody, err := io.ReadAll(srvrec.Response.Result().Body)
+	require.NoError(suite.T(), err)
+	require.Empty(suite.T(), recRespBody)
+}
+
+// Test HTTPTestServer when multiple predefined responses are defined. Test will ensure:
+//   - An empty 404 response is served when no predefined responses are available
+//   - PopServerRecord pops records and returns nil when no records are available
+//   - Server records and serves well multiple predefined repsonses in FIFO order.
+//   - When only one response is left, it is served indefinitly
+//   - ClearServerRecords clears the record queue
+//   - ClearPredefinedResponse clears the response queue
+func (suite *HTTPTestServerUnitTestSuite) TestWithMultipleResponses() {
+	// Get a HTTP client
+	client := suite.hts.Client()
+
+	// First, test a request when no predefined repsonse are set
+	resp, err := client.Get(suite.hts.GetBaseURL())
+	require.NoError(suite.T(), err)
+	require.NotNil(suite.T(), resp)
+	require.Equal(suite.T(), http.StatusNotFound, resp.StatusCode)
+	respBody, err := io.ReadAll(resp.Body)
+	require.NoError(suite.T(), err)
+	require.Empty(suite.T(), respBody)
+
+	// Pop server record twice. First record must contain the exchanged request-response
+	// and the second call must return a nil value
+	srvrec := suite.hts.PopServerRecord()
+	require.NotNil(suite.T(), srvrec)
+	require.Equal(suite.T(), http.StatusNotFound, srvrec.Response.Result().StatusCode)
+	srvrec = suite.hts.PopServerRecord()
+	require.Nil(suite.T(), srvrec)
+
+	// Push multiple predefined response
+	expectedStatusCode1 := http.StatusOK
+	expectedStatusCode2 := http.StatusNoContent
+	suite.hts.PushPredefinedServerResponse(&PredefinedServerResponse{
+		Status:  expectedStatusCode1,
+		Headers: map[string][]string{},
+		Body:    nil,
+	})
+	suite.hts.PushPredefinedServerResponse(&PredefinedServerResponse{
+		Status:  expectedStatusCode2,
+		Headers: map[string][]string{},
+		Body:    nil,
+	})
+
+	// Send a first request and ensure first response is served
+	resp, err = client.Get(suite.hts.GetBaseURL())
+	require.NoError(suite.T(), err)
+	require.NotNil(suite.T(), resp)
+	require.Equal(suite.T(), expectedStatusCode1, resp.StatusCode)
+
+	// Send a second request and ensure second response is served
+	resp, err = client.Get(suite.hts.GetBaseURL())
+	require.NoError(suite.T(), err)
+	require.NotNil(suite.T(), resp)
+	require.Equal(suite.T(), expectedStatusCode2, resp.StatusCode)
+
+	// Send a third request and ensure second served is still served
+	resp, err = client.Get(suite.hts.GetBaseURL())
+	require.NoError(suite.T(), err)
+	require.NotNil(suite.T(), resp)
+	require.Equal(suite.T(), expectedStatusCode2, resp.StatusCode)
+
+	// Ensure 3 server records are available then clear them and check
+	require.NotEmpty(suite.T(), suite.hts.records)
+	require.Len(suite.T(), suite.hts.records, 3)
+	suite.hts.ClearServerRecords()
+	require.Empty(suite.T(), suite.hts.records)
+
+	// Clear server responses and ensure an empty response is now served
+	require.NotEmpty(suite.T(), suite.hts.responses)
+	suite.hts.ClearPredefinedServerResponses()
+	require.Empty(suite.T(), suite.hts.responses)
+	resp, err = client.Get(suite.hts.GetBaseURL())
+	require.NoError(suite.T(), err)
+	require.NotNil(suite.T(), resp)
+	require.Equal(suite.T(), http.StatusNotFound, resp.StatusCode)
+}
+
+// Test HTTPServer with TLS enabled
+func (suite *HTTPTestServerUnitTestSuite) TestWithTLSEnabled() {
+	// Create a base httptest server
+	basetlssrv := httptest.NewUnstartedServer(nil)
+	require.NotNil(suite.T(), basetlssrv)
+	// Create a separate HTTPTestServer and plug the httptest.Server
+	srv := NewHTTPTestServer(basetlssrv)
+	require.NotNil(suite.T(), srv)
+	// Start the HTTPTestServer with TLS
+	srv.StartTLS()
+	defer srv.Close()
+	// Check BaseURL has https as scheme
+	require.Contains(suite.T(), srv.GetBaseURL(), "https")
+	// Test GetUnderlyingHTTPTestServer
+	tmp := srv.GetUnderlyingHTTPTestServer()
+	require.NotNil(suite.T(), tmp)
+	require.Equal(suite.T(), basetlssrv.Config.Addr, tmp.Config.Addr)
+	// Get a HTTP client with TLS settings
+	tlsclient := srv.Client()
+	// Send a request with TLS client and expect an empty 404 response from test server
+	resp, err := tlsclient.Get(srv.GetBaseURL())
+	require.NoError(suite.T(), err)
+	require.NotNil(suite.T(), resp)
+	require.Equal(suite.T(), http.StatusNotFound, resp.StatusCode)
+	// Create a default HTTP client
+	notlsclient := http.DefaultClient
+	// Send a request to base url and expect it to fail (remote errorr: tls: bad certificate)
+	resp, err = notlsclient.Get(srv.GetBaseURL())
+	require.Error(suite.T(), err)
+	require.Nil(suite.T(), resp)
+}
+
+// Test handleInternalError
+func (suite *HTTPTestServerUnitTestSuite) TestHandleInternalError() {
+	// Create a recorder to record response written by handler
+	rec := httptest.NewRecorder()
+	// Create a server record which will receive the provided error
+	srvrec := &ServerRecord{
+		Request:     nil,
+		Response:    nil,
+		RequestBody: &bytes.Buffer{},
+		ServerError: nil,
+	}
+	// Expected error
+	eerr := fmt.Errorf("PWNED")
+	// Use the error handler
+	suite.hts.handleInternalError(rec, srvrec, eerr)
+	// Check the server record and compare errors
+	require.Equal(suite.T(), eerr, srvrec.ServerError)
+	// Check response and expect 500 as status code + error string as text body
+	require.Equal(suite.T(), http.StatusInternalServerError, rec.Result().StatusCode)
+	require.Equal(suite.T(), "text/plain", rec.Result().Header.Get("Content-Type"))
+	recRespBody, err := io.ReadAll(rec.Result().Body)
+	require.NoError(suite.T(), err)
+	require.NotEmpty(suite.T(), recRespBody)
+	require.Equal(suite.T(), eerr.Error(), string(recRespBody))
+}
+
+// Test test server handler error paths
+func (suite *HTTPTestServerUnitTestSuite) TestServeHTTPErrPaths() {
+	// Create a mockReadCloser which fails when body is read
+	expectedErr := fmt.Errorf("PWNED")
+	mockedReadCloser := mockReadCloser{mock.Mock{}}
+	mockedReadCloser.
+		On("Read", mock.Anything).Return(0, expectedErr).
+		On("Close").Return(nil)
+	// Create a mockResponseWriter which fails when response is written
+	mockedResponseWriter := mockResponseWriter{mock.Mock{}}
+	mockedResponseWriter.
+		On("Header").Return(http.Header{}).
+		On("WriteHeader", mock.Anything).Return().
+		On("Write", mock.Anything).Return(0, expectedErr)
+	// Test server handler - fails when reading body
+	respRec := httptest.NewRecorder()
+	require.NotNil(suite.T(), respRec)
+	req := httptest.NewRequest(http.MethodPost, "/", &mockedReadCloser)
+	require.NotNil(suite.T(), req)
+	suite.hts.ServeHTTP(respRec, req)
+	require.Equal(suite.T(), http.StatusInternalServerError, respRec.Result().StatusCode)
+	// Test server handler - fails when parsing form data
+	respRec = httptest.NewRecorder()
+	require.NotNil(suite.T(), respRec)
+	req = httptest.NewRequest(http.MethodPost, "/", &mockedReadCloser)
+	// Set content type to form encoded so body is read by parseForm
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+	require.NotNil(suite.T(), req)
+	suite.hts.ServeHTTP(respRec, req)
+	require.Equal(suite.T(), http.StatusInternalServerError, respRec.Result().StatusCode)
+	// Create a multi target writer with a recorder and the mock which will fail write
+	respRec = httptest.NewRecorder()
+	require.NotNil(suite.T(), respRec)
+	mw := newMultiTargetHTTPResponseWriter(respRec, &mockedResponseWriter)
+	require.NotNil(suite.T(), mw)
+	// Create a GET request with no body to read
+	req = httptest.NewRequest(http.MethodGet, "/", nil)
+	require.NotNil(suite.T(), req)
+	// Set a predefined response with a body to write
+	suite.hts.PushPredefinedServerResponse(&PredefinedServerResponse{
+		Status: http.StatusOK,
+		Headers: map[string][]string{
+			"Content-Type": []string{"text/plain"},
+		},
+		Body: []byte("hello world!"),
+	})
+	// Clear server records
+	suite.hts.ClearServerRecords()
+	// Call ServeHTTP with the multi target writer
+	suite.hts.ServeHTTP(mw, req)
+	// Pop record and check error is set and has wrapped the expected error
+	record := suite.hts.PopServerRecord()
+	require.ErrorAs(suite.T(), record.ServerError, &expectedErr)
+}
+
+// Test MultiTargetResponseWriter Header method when no targets are set.
+//
+// This is to complete test coverage.
+func (suite *HTTPTestServerUnitTestSuite) TestMultiTargetResponseWriterHeaderWhenNoTargets() {
+	mw := newMultiTargetHTTPResponseWriter()
+	require.NotNil(suite.T(), mw)
+	headers := mw.Header()
+	require.NotNil(suite.T(), headers)
+	require.Equal(suite.T(), http.Header{}, headers)
+}
+
+/*************************************************************************************************/
+/* READCLOSER MOCK                                                                               */
+/*************************************************************************************************/
+
+// Mock for ReadCloser interface
+type mockReadCloser struct {
+	mock.Mock
+}
+
+// Mocked Read method
+func (m *mockReadCloser) Read(p []byte) (n int, err error) {
+	args := m.Called(p)
+	return args.Int(0), args.Error(1)
+}
+
+// Mocked Close method
+func (m *mockReadCloser) Close() error {
+	args := m.Called()
+	return args.Error(0)
+}
+
+// Test mockReadCloser complies to io.ReadCloser interface
+func TestMockReadCloserInterfaceCompliance(t *testing.T) {
+	var instance interface{} = &mockReadCloser{}
+	_, ok := instance.(io.ReadCloser)
+	require.True(t, ok)
+}
+
+/*************************************************************************************************/
+/* RESPONSE WRITER MOCK                                                                          */
+/*************************************************************************************************/
+
+// Mock for http.ResponseWriter interface
+type mockResponseWriter struct {
+	mock.Mock
+}
+
+// Mocked Header method
+func (m *mockResponseWriter) Header() http.Header {
+	args := m.Called()
+	return args.Get(0).(http.Header)
+}
+
+// Mocked Write method
+func (m *mockResponseWriter) Write(data []byte) (int, error) {
+	args := m.Called(data)
+	return args.Int(0), args.Error(1)
+}
+
+// Mocked WriteHeader method
+func (m *mockResponseWriter) WriteHeader(statusCode int) {
+	m.Called(statusCode)
+}
+
+// Test mockResponseWriter complies to http.ResponseWriter interface
+func TestMockResponseWriterInterfaceCompliance(t *testing.T) {
+	var instance interface{} = &mockResponseWriter{}
+	_, ok := instance.(http.ResponseWriter)
+	require.True(t, ok)
+}
